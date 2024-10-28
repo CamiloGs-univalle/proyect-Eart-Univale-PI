f44365834863023722c2bbe80390b11f76419c92
Camilo Gs Univalle <jhojan.garcia@correounivalle.edu.co>
2024-10-28 14:17:13 -0500

    jcgs
    login

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import "./index.css";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import Login from './Assets/Pages/Login/jsx/Login';
 
+
+
+const router = createBrowserRouter([
+  { path: "/", element: <Navigate to="/login" replace /> },
+  { path: "/login",element: <Login />,},
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
