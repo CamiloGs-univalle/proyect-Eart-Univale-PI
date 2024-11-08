8b990b7897db45b513a96ad8089a49c715ab93e6
Camilo Gs Univalle <jhojan.garcia@correounivalle.edu.co>
2024-11-08 10:32:50 -0500

    organizacion de la introduccion
    se acomodo el boton, y la margen del texto

--- src/Assets/Pages/Home/Introduction/Home.jsx
+++ src/Assets/Pages/Home/Introduction/Home.jsx
@@ -9,30 +9,35 @@ const Home = () => {
       <h1 className="title-earth">Tierra</h1>
       <div className="containerhome">
         <Earth3d className="container-3d-tierra" />
-        <div className="container-home">
-          <div className="text-container">
-            <p>
-            La Tierra enfrenta graves desafíos ambientales, como la deforestación, la erosión del suelo 
-            y la pérdida de biodiversidad, en gran parte causados por actividades humanas. Estos problemas 
-            no solo dañan el medio ambiente, sino que también amenazan el bienestar de las futuras generaciones. 
-            Además, el manejo inadecuado de residuos contamina ecosistemas y afecta la calidad de vida en muchas 
-            comunidades. Esta página explora cómo estos problemas están conectados, analiza sus causas y 
-            consecuencias, y propone soluciones prácticas para reducir su impacto. Aunque el planeta aún puede 
-            regenerarse, es esencial que tomemos medidas ahora para protegerlo.
-            </p>
-            <button className="btn">
-              <div className="wrapper">
-                <a href="/map" className="inicio">INICIAR PARTIDA</a>
-                {[...Array(6)].map((_, i) => (
-                  <div className={`flower flower${i + 1}`} key={i}>
-                    {[...Array(4)].map((_, j) => (
-                      <div className={`petal ${["one", "two", "three", "four"][j]}`} key={j} />
-                    ))}
-                  </div>
-                ))}
-              </div>
-            </button>
+        <div>
+
+          <div className="container-home">
+            <div className="text-container">
+              <p>
+                La Tierra enfrenta graves desafíos ambientales, como la deforestación, la erosión del suelo
+                y la pérdida de biodiversidad, en gran parte causados por actividades humanas. Estos problemas
+                no solo dañan el medio ambiente, sino que también amenazan el bienestar de las futuras generaciones.
+                Además, el manejo inadecuado de residuos contamina ecosistemas y afecta la calidad de vida en muchas
+                comunidades. Esta página explora cómo estos problemas están conectados, analiza sus causas y
+                consecuencias, y propone soluciones prácticas para reducir su impacto. Aunque el planeta aún puede
+                regenerarse, es esencial que tomemos medidas ahora para protegerlo.
+              </p>
+            </div>
           </div>
+          <button className="btn">
+            <div className="wrapper">
+              <a href="/map" className="inicio">INICIAR PARTIDA</a>
+              {[...Array(6)].map((_, i) => (
+                <div className={`flower flower${i + 1}`} key={i}>
+                  {[...Array(4)].map((_, j) => (
+                    <div className={`petal ${["one", "two", "three", "four"][j]}`} key={j} />
+                  ))}
+                </div>
+              ))}
+            </div>
+          </button>
+
+
         </div>
 
       </div>
