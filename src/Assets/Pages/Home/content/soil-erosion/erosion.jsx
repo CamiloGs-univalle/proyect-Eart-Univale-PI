ab3955a494f85a0e30ee4742c1530a3cedc3c30c
Camilo Gs Univalle <jhojan.garcia@correounivalle.edu.co>
2024-11-09 23:09:54 -0500

    erosion

---
+++ src/Assets/Pages/Home/content/soil-erosion/erosion.jsx
@@ -0,0 +1,47 @@
+import Header from "../../../Header/Header";
+import "./erosion.css";
+import Soil3D from "./model/Soil3D";
+
+const Erosion = () => {
+    return (
+        <>
+            <Header />
+            <div className="erosion-container">
+                <div>
+                    <h3 className="tituloSeccion">¿Cómo se produce la erosión del suelo?</h3>
+                    <p className="subtituloSeccion">La erosión del suelo es un proceso natural,
+                        pero en muchos casos acelerado por la actividad humana, que implica la
+                        pérdida y desplazamiento de la capa superior del suelo, esencial para la
+                        vida vegetal y la agricultura. Es un tema importante en el contexto ambiental,
+                        ya que afecta la productividad agrícola, la calidad del agua y la biodiversidad.
+                        A continuación te proporcionaré la información relevante y estructurada que
+                        necesitas para desarrollar una página web sobre este tema.</p>
+                </div>
+
+                <div className="conteiner">
+                    <div>
+                        <Soil3D className="tierragrieta" />
+                    </div>
+                    <div class="info-box">
+                        <p>La erosión del suelo es un proceso natural, pero en muchos casos
+                            acelerado por la actividad humana, que implica la pérdida y desplazamiento
+                            de la capa superior del suelo, esencial para la vida vegetal y la agricultura.
+                            Es un tema importante en el contexto ambiental, ya que afecta la productividad agrícola,
+                            la calidad del agua y la biodiversidad. A continuación te proporcionaré la información
+                            relevante y estructurada que necesitas para desarrollar una página web sobre este tema.</p>
+
+                        <h3>1. Definición de erosión del suelo</h3>
+
+                        <p>La erosión del suelo es el proceso por el cual las partículas de suelo son desplazadas
+                            y removidas, principalmente por el viento, el agua y, en menor medida, por
+                            actividades humanas como la agricultura y la construcción. A medida que la capa fértil
+                            del suelo se reduce, disminuye la capacidad de la tierra para soportar cultivos, ecosistemas
+                            y estructuras humanas.</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Erosion;
