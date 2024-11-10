50f623b055dc06c91b11fbe4b78f769e14a38fc2
Camilo Gs Univalle <jhojan.garcia@correounivalle.edu.co>
2024-11-10 00:12:43 -0500

    modelos 3d

--- src/Assets/Pages/Home/Map/model3D/Map3D.jsx
+++ src/Assets/Pages/Home/Map/model3D/Map3D.jsx
@@ -2,6 +2,7 @@
 import { Canvas } from "@react-three/fiber"; // Importa el componente Canvas que sirve como contenedor para renderizar el contenido 3D
 import { OrbitControls, useGLTF } from "@react-three/drei"; // Importa OrbitControls para permitir el control del modelo con el ratón y useGLTF para cargar modelos GLTF
 import ModelMap3D from "./ModelMap3D";
+import ModelMapSoil3D from "./ModelMapSoil3D";
 
 
 // Definición del componente funcional RecyCling
@@ -11,25 +12,26 @@ const Map3D = () => {
       {/* Contenedor para el modelo 3D con clase 'container-3d' */}
       <div className="container-map3d">
         {/* Canvas que actúa como el área para renderizar el contenido 3D */}
-        <Canvas
+        <Canvas className="map"
           camera={{
             position: [0, 70, 90], // Posición inicial de la cámara para que el modelo se vea completo
             fov: 70, // Campo de visión (Field of View) de la cámara, 70 grados
           }}>
-          
+
           {/* Controles para poder rotar el modelo 3D automáticamente */}
           <OrbitControls
             autoRotate={false} // El modelo rotará automáticamente
           />
-          
+
           {/* Luz ambiental que ilumina el modelo de manera uniforme */}
           <ambientLight />
-          
+
           {/* Luz direccional que ilumina desde una posición específica con una intensidad alta */}
           <directionalLight position={[10, 10, 10]} intensity={5} />
-          
+
           {/* Renderiza el modelo TrashCan, que contiene el modelo 3D */}
-          <ModelMap3D/>
+          <ModelMap3D />
+          
         </Canvas>
       </div>
     </>
