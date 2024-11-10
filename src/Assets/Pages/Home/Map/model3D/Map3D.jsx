053da2b11b03e6d46dc982451635b990bfa08407
Camilo Gs Univalle <jhojan.garcia@correounivalle.edu.co>
2024-11-10 18:03:09 -0500

    mapa + soil

--- src/Assets/Pages/Home/Map/model3D/Map3D.jsx
+++ src/Assets/Pages/Home/Map/model3D/Map3D.jsx
@@ -3,37 +3,31 @@ import { Canvas } from "@react-three/fiber"; // Importa el componente Canvas que
 import { OrbitControls, useGLTF } from "@react-three/drei"; // Importa OrbitControls para permitir el control del modelo con el ratón y useGLTF para cargar modelos GLTF
 import ModelMap3D from "./ModelMap3D";
 import ModelMapSoil3D from "./ModelMapSoil3D";
+import "../map.css"
 
 
 // Definición del componente funcional RecyCling
 const Map3D = () => {
   return (
     <>
-      {/* Contenedor para el modelo 3D con clase 'container-3d' */}
+
       <div className="container-map3d">
-        {/* Canvas que actúa como el área para renderizar el contenido 3D */}
-        <Canvas className="map"
-          camera={{
-            position: [0, 70, 90], // Posición inicial de la cámara para que el modelo se vea completo
-            fov: 70, // Campo de visión (Field of View) de la cámara, 70 grados
-          }}>
-
-          {/* Controles para poder rotar el modelo 3D automáticamente */}
-          <OrbitControls
-            autoRotate={false} // El modelo rotará automáticamente
-          />
-
-          {/* Luz ambiental que ilumina el modelo de manera uniforme */}
+        <Canvas className="map-canvas" camera={{ position: [0, 70, 90], fov: 70 }}>
+          {/* Luz ambiental */}
           <ambientLight />
+          <directionalLight position={[10, 10, 10]} intensity={3} />
 
-          {/* Luz direccional que ilumina desde una posición específica con una intensidad alta */}
-          <directionalLight position={[10, 10, 10]} intensity={5} />
+          {/* Modelo del mapa (quieto) */}
+          <ModelMap3D position={[0, 0, 0]} scale={[1, 1, 1]} />
 
-          {/* Renderiza el modelo TrashCan, que contiene el modelo 3D */}
-          <ModelMap3D />
-          
+          {/* Modelo del suelo (giratorio) */}
+          <ModelMapSoil3D position={[-60, 40, -30]} scale={[7, 7, 7]} />
+
+          {/* Controles de cámara */}
+          <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
         </Canvas>
       </div>
+
     </>
   );
 };
