b4a31007954f721a16a611d05548a195462a7142
Camilo Gs Univalle <jhojan.garcia@correounivalle.edu.co>
2024-11-19 10:59:46 -0500

    lll

--- src/Assets/Pages/Home/Map/map.jsx
+++ src/Assets/Pages/Home/Map/map.jsx
@@ -10,6 +10,7 @@ import Text3DSoil from "./model3D/Text3DSoil";
 import { Canvas } from "@react-three/fiber";
 import Text3DManagement from "./model3D/Text3DManagement";
 import { Link } from "react-router-dom";
+import FondoMap from "./FondoMap";
 
 const Map = () => {
   // Funciones de redirección
@@ -36,41 +37,8 @@ const Map = () => {
         <div>
           <h1>MAPA</h1>
           <div className="contenedormap">
-            <ModelMap3D className="container-map3d" />
+            <FondoMap className="container-map3d" />
 
-            <Canvas className="map-canvas" camera={{ position: [0, 70, 90], fov: 70 }}>
-              {/* Luz ambiental */}
-              <ambientLight />
-              <directionalLight position={[10, 10, 10]} intensity={3} />
-
-              {/* Modelo del mapa (quieto) */}
-              <ModelMap3D position={[0, 0, 0]} scale={[1, 1, 1]} />
-
-              {/* Grupo del suelo */}
-              <group position={[-60, 40, -30]}>
-                <ModelMapSoil3D position={[0, 10, -10]} scale={[5, 5, 5]} onClick={redirectToSoil} />
-                <Text3DSoil />
-              </group>
-
-              {/* Grupo de gestión */}
-              <group position={[0, 20, 20]}>
-                <ModelMapManagement3D position={[0, 5, -10]} scale={[4, 4, 4]} onClick={redirectToManagement} />
-                <Text3DManagement />
-              </group>
-
-              {/* Grupo de deforestación */}
-              <group position={[20, 40, -30]}>
-                <ModelMapDeforestation3D position={[0, 0, -10]} scale={[4, 4, 4]} onClick={redirectToDeforestation} />
-              </group>
-
-              {/* Grupo de biodiversidad */}
-              <group position={[70, 10, 20]}>
-                <ModelMapBiodiversity3D position={[0, 0, -10]} scale={[4, 4, 4]} onClick={redirectToBiodiversity} />
-              </group>
-
-              {/* Controles de cámara */}
-              <OrbitControls enablePan={false} enableZoom={false} enableRotate={true} />
-            </Canvas>
           </div>
         </div>
       </div>
