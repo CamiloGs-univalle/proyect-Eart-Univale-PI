61fa9f959eade9b09239af2d599f43726cc122a1
Camilo Gs Univalle <jhojan.garcia@correounivalle.edu.co>
2024-11-30 22:46:36 -0500

    mejora

---
+++ src/Assets/Pages/Home/content/soil-erosion/contenido/Contenido.jsx
@@ -0,0 +1,54 @@
+import "./contenido.css";
+
+export const erosion = (
+    <>
+        <h1 className="titulo">Erosión del Suelo</h1>
+        <div className="erosion-container">
+            <div>
+                <h3 className="tituloSeccion">¿Cómo se produce la erosión del suelo?</h3>
+                <p className="subtituloSeccion">La erosión del suelo es un proceso natural,
+                    pero en muchos casos acelerado por la actividad humana, que implica la
+                    pérdida y desplazamiento de la capa superior del suelo, esencial para la
+                    vida vegetal y la agricultura. Es un tema importante en el contexto ambiental,
+                    ya que afecta la productividad agrícola, la calidad del agua y la biodiversidad.
+                    A continuación te proporcionaré la información relevante y estructurada que
+                    necesitas para desarrollar una página web sobre este tema.</p>
+            </div>
+        </div>
+    </>
+);
+
+export const infoErosion = (
+    <>
+        <div class="info-box">
+            <h3>Definición de erosión del suelo</h3>
+            <p>
+                La erosión del suelo es un proceso natural, pero en muchos casos
+                acelerado por la actividad humana, que implica la pérdida y desplazamiento
+                de la capa superior del suelo, esencial para la vida vegetal y la agricultura.
+                Es un tema importante en el contexto ambiental, ya que afecta la productividad agrícola,
+                la calidad del agua y la biodiversidad. A continuación te proporcionaré la información
+                relevante y estructurada que necesitas para desarrollar una página web sobre este tema.
+            </p>
+            <p>
+                La erosión del suelo es el proceso por el cual las partículas de suelo son desplazadas
+                y removidas, principalmente por el viento, el agua y, en menor medida, por
+                actividades humanas como la agricultura y la construcción. A medida que la capa fértil
+                del suelo se reduce, disminuye la capacidad de la tierra para soportar cultivos, ecosistemas
+                y estructuras humanas.
+            </p>
+        </div>
+    </>
+);
+
+export const infoTiposErosion = (
+    <>
+        <div class="info-box">
+            <h3>Tipos de erosión del suelo</h3>
+            <p>Erosión hídrica (por agua) El agua es una de las principales causas de erosión del suelo. Este tipo de erosión ocurre cuando la lluvia y el agua corriente arrastran las partículas de suelo, lo que puede generar varios tipos de erosión hídrica:</p>
+        </div>
+    </>
+);
+
+
+
