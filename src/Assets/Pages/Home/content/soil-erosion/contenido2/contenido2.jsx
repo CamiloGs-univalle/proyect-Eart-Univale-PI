6d8568e0c3226beb89288554a0964490d5b4c69e
Camilo Gs Univalle <jhojan.garcia@correounivalle.edu.co>
2024-12-11 23:36:07 -0500

    music

--- src/Assets/Pages/Home/content/soil-erosion/contenido2/contenido2.jsx
+++ src/Assets/Pages/Home/content/soil-erosion/contenido2/contenido2.jsx
@@ -1,4 +1,24 @@
 import "./contenido2.css"
+import React, { useRef, useEffect } from "react";
+
+export default function MusicaAmbient() {
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    // Autoplay cuando el componente se monta
+    audioRef.current.play().catch(() => {
+      console.log("Autoplay bloqueado por el navegador, espera interacción.");
+    });
+  }, []);
+
+  return (
+    <audio ref={audioRef} loop>
+      <source src="/music/musicAmbient.mp3" type="audio/mpeg" />
+      Tu navegador no soporta el audio.
+    </audio>
+  );
+}
+
 export const Agua = (
     <div className="sections">
         <h1 className="titulo">Consecuencias y soluciones</h1>
